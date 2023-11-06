8f728dba4f3c9e44c332fe3fc5ad33b70338c0fc
Aida <aidasufaj00@gmail.com>
2023-11-06 17:43:58 +0100

    install uuid

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,5 +1,19 @@
+import { FaAngleDoubleRight } from "react-icons/fa";
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Duties({ duties }) {
-  return <div></div>;
+  return (
+    <div>
+      {duties.map((duty, index) => {
+        const id = uuidv4();
+        return (
+          <div key={index} className="job-desc">
+            <FaAngleDoubleRight className="job-icon" />
+            <p>{duty}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
